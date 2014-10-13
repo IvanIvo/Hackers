fba93fc27ef45dfdfe35f50856faa160069651af
igachmov <ivan.gachmov@abv.bg>
2014-10-13 17:17:03 +0300

    eee nai nakraq

--- Friends/src/testClass.java
+++ Friends/src/testClass.java
@@ -9,6 +9,10 @@ public class testClass {
 		System.out.println("ebi mu maikataaa");
 		System.out.println("ebi mu q be debel");
 		System.out.println("ebasi tupoto debel");
+		System.out.println("ebasi tupoto debelako");
+		System.out.println("e sega mu ebah maikata");
+		System.out.println("e sega mu ebah maikatasdasda");
+		System.out.println("e eeee nai nakraq");
 	}
 
 }
